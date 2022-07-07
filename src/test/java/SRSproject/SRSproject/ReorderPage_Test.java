5f50bb562e4f6cb841d9230d82192db68dbee7ce
Marirajp <101637497+Marirajp@users.noreply.github.com>
2022-07-07 11:19:46 +0530

    Update ReorderPage_Test.java

--- src/test/java/SRSproject/SRSproject/ReorderPage_Test.java
+++ src/test/java/SRSproject/SRSproject/ReorderPage_Test.java
@@ -276,7 +276,7 @@ public class ReorderPage_Test  extends BaseTest
 		String Success_msg= Rp.Items_addtocart_msg();
 		Thread.sleep(5000);
 		 System.out.println(Success_msg);
-		 Thread.sleep(2000);
+		 Thread.sleep(4000);
 		Assert.assertEquals(Success_msg.contains(" Items have been successfully added to your Cart"), true);
 //        System.out.println(Success_msg);
  
